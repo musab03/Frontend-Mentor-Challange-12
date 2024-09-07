fab21e37a056629c797cc486a46e7605b1f92dec
musab03 <musab.khurram@gmail.com>
2024-09-07 16:25:58 +0500

    fix: position

--- src/App.tsx
+++ src/App.tsx
@@ -32,16 +32,26 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-row w-full max-w-4xl mx-auto mt-40 rounded-lg overflow-hidden ">
-        {Cars.map((car) => (
-        <div className={`${car.bgcolor} py-6 px-12 w-80` }>
-          <img src={car.icon} alt="sedan-icon" />
-          <h2 className="my-8 font-semibold text-3xl text-white" >{car.model}</h2>
-          <p className=" text-white ">{car.detail}</p>
-  <button className={`mt-16 bg-white rounded-full px-6 py-2 hover:bg-transparent hover:text-white hover:border-2 ${car.textcolor} border-2 border-white  mb-4`}>Learn More</button>
-        </div>
+      <div className="flex md:flex-row flex-col min-w-sm  w-full max-w-4xl mx-auto mt-40 rounded-lg overflow-hidden  ">
+        {Cars.map((car, index) => (
+          <div
+            key={index}
+            className={`${car.bgcolor} py-6 px-12 w-80 md:rounded-none ${
+              index === 0 ? "rounded-t-lg" : ""
+            } ${index === 2 ? "rounded-b-lg" : ""} `}
+          >
+            <img src={car.icon} alt="sedan-icon" />
+            <h2 className="my-8 font-semibold text-3xl text-white">
+              {car.model}
+            </h2>
+            <p className=" text-white ">{car.detail}</p>
+            <button
+              className={`mt-16 bg-white rounded-full px-6 py-2 hover:bg-transparent hover:text-white hover:border-2 ${car.textcolor} border-2 border-white  mb-4`}
+            >
+              Learn More
+            </button>
+          </div>
         ))}
-        
       </div>
     </>
   );
